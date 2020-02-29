e3974d91efb51251c1c39df7353d9c7d1ee0385f
challangerD <59607064+challangerD@users.noreply.github.com>
2020-02-29 23:12:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
+from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
 
-last_indeed_page = extract_indeed_pages()
-
-extract_indeed_jobs(last_indeed_page)
-
-indeed_jobs = extract_indeed_jobs(last_indeed_page)
+indeed_jobs = get_indeed_jobs()
+so_jobs =  get_so_jobs()
