09d1e2fc93ccc9a0d0f0e6f043b91449cda13b22
challangerD <59607064+challangerD@users.noreply.github.com>
2020-03-01 19:26:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from indeed import get_jobs as get_indeed_jobs
 from so import get_jobs as get_so_jobs
+from save import save_to_file
 
 indeed_jobs = get_indeed_jobs()
 so_jobs =  get_so_jobs()
 jobs = indeed_jobs + so_jobs
-print(jobs)
+save_to_file(jobs)
+
