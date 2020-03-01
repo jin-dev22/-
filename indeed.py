8702795f80c1b1b09884d02c24a552abf354db43
challangerD <59607064+challangerD@users.noreply.github.com>
2020-03-01 16:53:58 +0900

    Update indeed.py

--- indeed.py
+++ indeed.py
@@ -6,13 +6,9 @@ URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
 
 def get_last_page():
-
     result = requests.get(URL)
-
     soup = BeautifulSoup(result.text, "html.parser")
-
     pagination = soup.find("div", {"class": "pagination"})
-
     links = pagination.find_all('a')
     pages = []
     for link in links[:-1]:
@@ -26,7 +22,6 @@ def extract_job(html):
     company = html.find("span", {"class": "comapny"})
     if company:
         company_anchor = company.find("a")
-
         if company_anchor is not None:
             company = str(company_anchor.string)
         else:
@@ -42,20 +37,20 @@ def extract_job(html):
     }
 
 
-
 def extract_jobs(last_page):
-  jobs = []
-  for page in range(last_page):
-    print(f"Scrappping Page {page}")
-    result = requests.get(f"{URL}&start={page*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-    for result in results:
-        job = extract_job(result)
-        jobs.append(job)
-  return jobs
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrappping Indeed Page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
-def get_jobs():  
-  last_page = get_last_page()
-  jobs = extract_jobs(last_page)
-  return jobs
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
+    return jobs
