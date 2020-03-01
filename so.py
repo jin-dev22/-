0d590492b2a9c350e431b05f9ef10b8d3a515b11
challangerD <59607064+challangerD@users.noreply.github.com>
2020-03-01 15:30:21 +0900

    Update so.py

--- so.py
+++ so.py
@@ -3,23 +3,40 @@ from bs4 import BeautifulSoup
 
 URL = f"https://stackoverflow.com/jobs?q=python&sort=i&pg=2"
 
+
 def get_last_page():
-  result = requests.get(URL)
-  soup = BeautifulSoup(result.text,"html.parser")
-  pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
-  pages = pages[0:-1]
-  last_page = pages[-1].get_text(strip =True)
-  return int(last_page)
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    pages = pages[0:-1]
+    last_page = pages[-1].get_text(strip=True)
+    return int(last_page)
+
+
+def extract_job(html):
+    title = html.find("div", {
+        "class": "grid--cell fl1"
+    }).find("h2").find("a")["title"]
+    company, location = html.find("h3", {
+        "class": "fc-black-700"
+    }).find_all(
+        "span", recursive=False)
+    print(company.get_text(strip=True), location.get_text(strip=True))
+    #return {'title':title}
 
 def extract_jobs(last_page):
-  jobs = []
-  for page in range(last_page):
-    result = requests.get(f"{URL}&pg={page+1}")
-    soup = BeautifulSoup(result.txt, "html.parser")
-    
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "-job"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
 
 def get_jobs():
-  last_page = get_last_page()
-  jobs = extract_jobs(last_page)
-  return jobs
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
+    return jobs
